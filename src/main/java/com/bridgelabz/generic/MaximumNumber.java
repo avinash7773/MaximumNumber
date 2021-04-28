873b4383062c493c056ed024db826fa9933678b1
avinash7773 <javinash228@gmail.com>
2021-04-28 21:58:14 +0530

    add to check maximum float number of first position from given float numbers

--- src/main/java/com/bridgelabz/generic/MaximumNumber.java
+++ src/main/java/com/bridgelabz/generic/MaximumNumber.java
@@ -3,8 +3,19 @@ package com.bridgelabz.generic;
 public class MaximumNumber {
 
     //@Method maximumIntegerNumber check maximum integer from given integer numbers
-    public static int maximumIntegerNumber(Integer first_number, Integer second_number, Integer third_number) {
+    public  int maximumIntegerNumber(Integer first_number, Integer second_number, Integer third_number) {
         int maximum = first_number;
+        if (second_number.compareTo(maximum) > 0)
+            maximum = second_number;
+        if (third_number.compareTo(maximum) > 0)
+            maximum = third_number;
+        System.out.println("Maximum number=" + maximum);
+        return maximum;
+    }
+
+    //@Method maximumIntegerNumber check maximum integer from given integer numbers
+    public float maximumFloatNumber(Float first_number,Float second_number, Float third_number){
+        float maximum = first_number;
         if (second_number.compareTo(maximum) > 0)
             maximum = second_number;
         if (third_number.compareTo(maximum) > 0)
--- src/test/java/com/bridgelabz/generic/MaximumNumberTest.java
+++ src/test/java/com/bridgelabz/generic/MaximumNumberTest.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.generic;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,10 +13,17 @@ public class MaximumNumberTest {
         maximumNumber = new MaximumNumber();
     }
 
-    //@testMaximumIntegerNumber check for third position number
+    //@testMaximumIntegerNumber check for third position maximum number
     @Test
     void testMaximumIntegerNumber() {
         int result = maximumNumber.maximumIntegerNumber(4,5,8);
         Assert.assertEquals(8,result);
+    }
+
+    //@testMaximumFloatNumber check for first position  maximum number
+    @Test
+    void testMaximumFloatNumber() {
+        float result = maximumNumber.maximumFloatNumber(5.4f,6.67f,2.4f);
+        Assertions.assertEquals(6.67f,result);
     }
 }
